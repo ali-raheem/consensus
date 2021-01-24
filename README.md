0d3d72bec9225ab132069dd75f08e8bbac1fc64f
Ali Raheem <github@shoryuken.me>
2021-01-24 13:22:09 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Consensus
+
+Small library for multiple parties to estbalish a shared random number. Even if you are the only honest party.
+
+## Algorithim
+
+1. All parties pick a secret random number
+2. Each party shares a hash of that random number
+3. Each party reveals the random number
+4. Each party ensures the hashes matches the revealed number
+5. All the random numbers are bitwise XOR'd to produce a random number
+
+## Dependencies
+
+- libtomcrypt
+
+## Build
+
+```
+gcc -o bin/consensus_test src/test.c src/consensus.c -static -ltomcrypt
+```
+
+## Usage
+
+See `src/test.c`, each party only generates one secret, disseminating the hash, and then the secret is left up to the user.
---
+++ src/test.c
@@ -0,0 +1,41 @@
+#include "consensus.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <sys/mman.h>
+
+int main(int argc, char* argv[]) {
+  assert(CONSENSUS_INIT_OK == consensus_init());
+
+  const size_t count = 16;
+  unsigned char secrets[count][CONSENSUS_SECRET_LENGTH];
+  unsigned char hashes[count][CONSENSUS_HASH_LENGTH];
+  int i;
+  for(i = 0; i < count; i++) {
+    assert(CONSENSUS_RNG_OK == consensus_generate_secret(secrets[i], hashes[i]));
+    int j;
+    printf("Secret:\t");
+    for(j= 0; j < CONSENSUS_SECRET_LENGTH; j++)
+      printf("%02X", secrets[i][j]);
+    printf("\nHash:\t");
+    for(j= 0; j < CONSENSUS_HASH_LENGTH; j++)
+      printf("%02X", hashes[i][j]);
+    printf("\n\n");
+  }
+
+  //  mprotect(secrets, CONSENSUS_SECRET_LENGTH*count, PROT_READ | PROT_WRITE);
+  puts("Check hashes");
+  assert(CONSENSUS_CHECK_OK == consensus_check_hashes(count, hashes));
+  puts("Verify hashes");
+  assert(CONSENSUS_VERIFY_OK == consensus_verify_secrets(count, hashes, secrets));
+
+  unsigned char shared[CONSENSUS_SECRET_LENGTH];
+  puts("Generate shared");
+  assert(CONSENSUS_GENERATE_OK == consensus_generate_shared(count, secrets, shared));
+  for(i = 0; i < CONSENSUS_SECRET_LENGTH; i++)
+    printf("%2X", shared[i]);
+  puts("");
+  
+}
